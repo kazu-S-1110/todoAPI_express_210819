5efb02eda0ff41b7cc2ab00bd9a457987178645c
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-20 12:37:34 +0900

    routeを定義。

---
+++ api/routes/taskRoutes.js
@@ -0,0 +1,11 @@
+module.exports = (app) => {
+  var taskList = require('../controllers/taskController');
+
+  app.route('/tasks').get(taskList.all_tasks).post(taskList.create_task);
+
+  app
+    .route('/tasks/:taskId')
+    .get(taskList.load_task)
+    .put(taskList.update_task)
+    .delete(taskList.delete_task);
+};
